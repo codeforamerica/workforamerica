4bd23c94aa5310f28cc34d9e40a754d23f2c7f85
amaier <amaier@gettyimages.com>
2014-04-28 17:00:28 -0700

    Move to html

--- _header.php
+++
@@ -1,8 +0,0 @@
-<header role="masthead">
-  <nav>
-    <a href="index.php" class="home">Work for America</a>
-    <a href="civic.php" class="civi-startups">Civic startups</a>
-    <a href="governments.php" class="governments">Governments</a>
-    <a href="suggest.php" class="suggest-a-job">Suggest a job</a>
-  </nav>
-</header>
--- _html_head.php
+++
@@ -1,4 +0,0 @@
-<head>
-  <title>Work for America</title>
-  <link rel="stylesheet" type="text/css" href="stylesheets/styles.css">
-</head>
